7d265a6227baf1a2adc2cd1b63384bfb8a8c5dfc
Mathieu Poumeyrol <kali@zoy.org>
2017-02-12 15:47:49 +0100

    try out an addmul_2

--- src/ll/mul.rs
+++ src/ll/mul.rs
@@ -259,6 +259,84 @@ pub unsafe fn addmul_1(wp: LimbsMut, xp: Limbs, n: i32, vl: Limb) -> Limb {
     Limb(r as _)
 }
 
+#[inline(always)]
+#[allow(dead_code)]
+pub unsafe fn _addmul_2(mut wp: LimbsMut, mut xp: Limbs, mut n: i32, vl1: Limb, vl2:Limb) -> (Limb, Limb) {
+    debug_assert!(n > 0);
+    debug_assert!(same_or_separate(wp, n, xp, n));
+
+    let mut carry_1 = Limb(0);
+    let mut carry_2 = Limb(0);
+    loop {
+        let (hi_l2, lo_l2) = (*xp).mul_hilo(vl2);
+        let (hi_l1, lo_l1) = (*xp).mul_hilo(vl1);
+
+        let (s, cl1) = (*wp).add_overflow(lo_l1);
+        let (s, cl2) = s.add_overflow(carry_1);
+        *wp = s;
+
+        let (s, cl1) = carry_2.add_overflow(Limb(cl1 as _) + Limb(cl2 as _));
+        let (s, cl2) = s.add_overflow(lo_l2);
+        let (s, cl3) = s.add_overflow(hi_l1);
+        carry_1 = s;
+
+        carry_2 = hi_l2 + (Limb(cl1 as _) + Limb(cl2 as _) + Limb(cl3 as _));
+
+        n -= 1;
+        if n == 0 { break; }
+        xp = xp.offset(1);
+        wp = wp.offset(1);
+    }
+    (carry_1, carry_2)
+}
+
+#[inline(always)]
+#[allow(dead_code)]
+#[allow(unused_assignments)]
+pub unsafe fn addmul_2(wp: LimbsMut, xp: Limbs, n: i32, vl1: Limb, vl2:Limb) -> (Limb, Limb) {
+    debug_assert!(n > 0);
+    debug_assert!(same_or_separate(wp, n, xp, n));
+    let mut n:i64 = n as _;
+    let mut w:*mut _ = &mut *wp.offset(0);
+    let mut x:*const _ = &*xp.offset(0);
+
+    let mut carry_1 = 0u64;
+    let mut carry_2 = 0u64;
+    asm!("
+    mov ($3), %rbx
+    lea ($2,$4,8), $2       // $2 is wp
+    lea ($3,$4,8), $3       // $3 is xp
+    neg $4                  // $4 is n
+
+    .align 4
+    1: 
+    xor %r8, %r8
+    mov 8($3,$4,8), %rbx
+    mov %rbx, %rax
+    mul $10                 // $10 is vl1
+    add %rax, $0
+    adc %rdx, $1
+    adc $$0, %r8            // temps: %r8:$1:$0
+    add $0, ($2,$4,8)       // write wi
+    adc $$0, $1
+    adc $$0, %r8
+    mov $1, $0
+    mov %r8, $1
+    mov %rbx, %rax
+    mov 8($3,$4,8), %rbx
+    mul $11                 // $11 is vl2
+    add %rax, $0
+    adc %rdx, $1
+
+    add $$1, $4
+    js 1b
+    "
+    : "=&r"(carry_1), "=&r"(carry_2), "=&r"(w), "=&r"(x), "=&r"(n)
+    : "0"(carry_1), "1"(carry_2), "2"(w), "3"(x), "4"(n), "r"(vl1.0), "r"(vl2.0)
+    : "r8", "rax", "rbx", "rdx", "memory", "cc");
+    (Limb(carry_1), Limb(carry_2))
+}
+
 #[inline]
 #[allow(dead_code)]
 unsafe fn submul_1_generic(mut wp: LimbsMut, mut xp: Limbs, mut n: i32, vl: Limb) -> Limb {
@@ -409,18 +487,22 @@ pub unsafe fn mul(wp: LimbsMut, xp: Limbs, xs: i32, yp: Limbs, ys: i32) {
 
 #[inline(always)]
 unsafe fn mul_basecase(mut wp: LimbsMut, xp: Limbs, xs: i32, mut yp: Limbs, mut ys: i32) {
-
     *wp.offset(xs as isize) = ll::mul_1(wp, xp, xs, *yp);
     wp = wp.offset(1);
     yp = yp.offset(1);
     ys -= 1;
 
-    while ys > 0 {
-        *wp.offset(xs as isize) = ll::addmul_1(wp, xp, xs, *yp);
+    while ys > 1 {
+        let (c1, c2) = addmul_2(wp, xp, xs, *yp, *yp.offset(1));
+        *wp.offset(xs as isize) = c1;
+        *wp.offset(xs as isize + 1) = c2;
 
-        wp = wp.offset(1);
-        yp = yp.offset(1);
-        ys -= 1;
+        wp = wp.offset(2);
+        yp = yp.offset(2);
+        ys -= 2;
+    }
+    if ys == 1 {
+        *wp.offset(xs as isize) = ll::addmul_1(wp, xp, xs, *yp);
     }
 }
 
@@ -743,7 +825,66 @@ mod test {
         }
     }
 
-    macro_rules! one_bench_named {
+    #[test]
+    fn test_addmul_2() {
+        use super::addmul_2;
+        use ll::limb_ptr::{Limbs, LimbsMut};
+        use ll::limb::Limb;
+        unsafe {
+            for &(w, x, l1, l2, exp, exp_c) in &[
+                (&[0usize, 0], &[0usize, 0], 0, 0, &[0usize, 0], (0, 0)),
+                (&[1, 0], &[0, 0], 1, 0, &[1, 0], (0,0)),
+                (&[0, 1], &[0, 0], 1, 0, &[0, 1], (0,0)),
+                (&[0, 0], &[0, 0], 1, 0, &[0, 0], (0,0)),
+                (&[0, 0], &[1, 0], 1, 0, &[1, 0], (0,0)),
+                (&[1, 0], &[1, 0], 1, 0, &[2, 0], (0,0)),
+                (&[0, 1], &[1, 0], 1, 0, &[1, 1], (0,0)),
+                (&[0, 0], &[1, 0], 1, 0, &[1, 0], (0,0)),
+                (&[0, 0], &[1, 0], 0, 1, &[0, 1], (0,0)),
+                (&[0, 0], &[0, 1], 0, 1, &[0, 0], (1,0)),
+                (&[0, 0], &[!0, !0], !0, !0, &[1, 0], (!0-1, !0)),
+            ] {
+                let x_vec = x.to_vec();
+                let x_limbs = Limbs::new(x_vec.as_ptr() as _, 0, x.len() as i32);
+                let w_vec = w.to_vec();
+                let w_limbs = LimbsMut::new(w_vec.as_ptr() as _, 0, w_vec.len() as i32);
+                let (Limb(c1), Limb(c2)) = addmul_2(w_limbs, x_limbs, x.len() as _, Limb(l1), Limb(l2));
+                assert_eq!(exp, &*w_vec,
+                           "wrong result testing {:?} += {:?} * {}:{}", w, x, l2, l1);
+                assert_eq!(exp_c, (c1, c2),
+                           "wrong carry testing {:?} += {:?} * {}:{}", w, x, l2, l1);
+            }
+        }
+    }
+
+    #[test]
+    fn test_mul_basecase() {
+        use ll::limb_ptr::{Limbs, LimbsMut};
+        unsafe {
+            for &(x, y, exp) in &[
+                (&[0usize, 0] as &[usize], &[0usize, 0] as &[usize], &[0usize, 0, 0, 0] as &[usize]),
+                (&[1, 0], &[1, 0], &[1usize, 0, 0, 0]),
+                (&[!0, !0], &[1, 0], &[!0, !0, 0, 0]),
+                (&[!0, !0], &[!0, !0], &[1, 0, !0-1, !0]),
+                (&[!0, !0, !0], &[!0, !0, !0], &[1, 0, 0, !0-1, !0, !0]),
+                (&[1], &[1, 2, 3], &[1, 2, 3, 0]),
+                (&[1], &[1, 2, 3, 4], &[1, 2, 3, 4, 0]),
+                (&[0, 2], &[1, 2, 3, 4], &[0, 2, 4, 6, 8, 0]),
+            ] {
+                let x_vec = x.to_vec();
+                let y_vec = y.to_vec();
+                let w_vec = vec!(0usize; x.len()+y.len());
+                let x_limbs = Limbs::new(x_vec.as_ptr() as _, 0, x.len() as i32);
+                let y_limbs = Limbs::new(y_vec.as_ptr() as _, 0, y.len() as i32);
+                let w_limbs = LimbsMut::new(w_vec.as_ptr() as _, 0, w_vec.len() as i32);
+                super::mul_basecase(w_limbs, x_limbs, x.len() as _, y_limbs, y.len() as _);
+                assert_eq!(exp, &*w_vec,
+                           "wrong result testing {:?}*{:?}={:?} ", x, y, w_vec);
+            }
+        }
+    }
+
+    macro_rules! one_bench {
         ($size:expr, $name:ident, $what:expr) => {
             #[bench]
             fn $name(b: &mut ::test::Bencher) {
@@ -767,23 +908,23 @@ mod test {
     }
 
     macro_rules! ladder { ($what:expr) => {
-        one_bench_named!(1, size_0001, $what);
-        one_bench_named!(2, size_0002, $what);
-        one_bench_named!(4, size_0004, $what);
-        one_bench_named!(8, size_0008, $what);
-        one_bench_named!(16, size_0016, $what);
-        one_bench_named!(32, size_0032, $what);
-        one_bench_named!(64, size_0064, $what);
-        one_bench_named!(128, size_0128, $what);
-        one_bench_named!(256, size_0256, $what);
-        one_bench_named!(512, size_0512, $what);
-        one_bench_named!(1024, size_1024, $what);
-        one_bench_named!(2048, size_2048, $what);
-        one_bench_named!(4096, size_4096, $what);
-        one_bench_named!(8148, size_8148, $what);
+        one_bench!(2, size_0002, $what);
+        one_bench!(4, size_0004, $what);
+        one_bench!(8, size_0008, $what);
+        one_bench!(16, size_0016, $what);
+        one_bench!(32, size_0032, $what);
+        one_bench!(64, size_0064, $what);
+        one_bench!(128, size_0128, $what);
+        one_bench!(256, size_0256, $what);
+        one_bench!(512, size_0512, $what);
+        one_bench!(1024, size_1024, $what);
+        one_bench!(2048, size_2048, $what);
+        one_bench!(4096, size_4096, $what);
+        one_bench!(8192, size_8192, $what);
     }}
 
     mod mul_1 { ladder!(|z,x,xs,y:Limbs| super::super::mul_1(z, x, xs as i32, *y)); }
     mod addmul_1 { ladder!(|z,x,xs,y:Limbs| super::super::addmul_1(z, x, xs as i32, *y)); }
+    mod addmul_2 { ladder!(|z,x,xs,y:Limbs| super::super::addmul_2(z, x, xs as i32, *y, *y.offset(1))); }
     mod mul_basecase { ladder!(|z,x,xs,y| super::super::mul_basecase(z, x, xs as i32, y, xs as i32)); }
 }
